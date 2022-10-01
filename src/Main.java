0819893f7cee20fe7efbadef51b82685a8b96340
Lilnurs <zulpukarov518@icloud.com>
2022-10-01 19:16:15 +0600

    hw1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        String iAm;
+        final int num = 18;
+        String word = "hello";
+        iAm = num + " " + word;
+        System.out.println(iAm);
+
+        if (num < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        } else if (num > 0) {
+            System.out.println("Вы сохранили полажительное число");
+
+        }else {
+            System.out.println("Вы сохранили 0");
+        }
+
+    }
+}
