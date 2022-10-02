e1ac2df5482b3e23e55a819eb4cf7890ab69ce34
Lilnurs <zulpukarov518@icloud.com>
2022-10-02 18:08:31 +0600

    hw1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Hello " + " Nursultan");
         String iAm;
         final int num = 18;
         String word = "hello";
